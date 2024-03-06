e046f16866b03f5495758a0a72355b85b0915dbe
Parthiv <parthivsen47@gmail.com>
2024-03-06 14:48:30 +0530

    first commit

---
+++ prediction.py
@@ -0,0 +1,31 @@
+import numpy as np
+from keras.models import load_model
+from ela import convert_to_ela_image
+
+
+def prepare_image(fname):
+    image_size = (128, 128)
+    return (
+        np.array(convert_to_ela_image(
+            fname[0], 90).resize(image_size)).flatten()
+        / 255.0
+    )  # return ela_image as a numpy array
+
+
+def predict_result(fname):
+    # load the trained model
+    model = load_model(
+        r"C:\Users\parth\Downloads\cnn\image-forgery-detection-main\model_test.h5")
+    class_names = ["Forged", "Authentic"]  # classification outputs
+    test_image = prepare_image(fname)
+    test_image = test_image.reshape(-1, 128, 128, 3)
+
+    y_pred = model.predict(test_image)
+    y_pred_class = round(y_pred[0][0])
+
+    prediction = class_names[y_pred_class]
+    if y_pred <= 0.5:
+        confidence = f"{(1-(y_pred[0][0])) * 100:0.2f}"
+    else:
+        confidence = f"{(y_pred[0][0]) * 100:0.2f}"
+    return (prediction, confidence)
